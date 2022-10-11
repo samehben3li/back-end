ed24404492c054af06e1ea8358e2cbaac927da11
sameh <sameh@ranadev.io>
2022-10-11 10:02:04 +0100

    add .gitlab_ci/cd file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import { ApolloServer } from "apollo-server"
+import express from "express"
 import * as dotenv from 'dotenv'
 import * as mongoose from "mongoose"
 import {typeDefs} from "./schema/typeDefs"
 import {resolvers} from "./schema/resolvers"
 
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 4000
+app.use(express.json())
 
 const server = new ApolloServer({
     typeDefs,resolvers,
     context: ({req,res}:any) => ({req,res})
 })
 
-dotenv.config()
 
 const connect = async() => {
     try {
