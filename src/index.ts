6bf9daa0613527d53b7cf493f9f742b0067a7046
sameh <sameh@ranadev.io>
2022-10-20 23:28:58 +0100

    set public folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { ApolloServer } from 'apollo-server';
-import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
+import * as path from 'path';
+import express from 'express';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import typeDefs from './schema/typeDefs';
 import resolvers from './schema/resolvers';
@@ -10,6 +11,7 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 4000;
 app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 const server = new ApolloServer({
   typeDefs,
@@ -23,11 +25,16 @@ const server = new ApolloServer({
 const connect = async () => {
   await mongoose.connect(process.env.MONGO_URI as string);
   console.log('connected to database');// eslint-disable-line
-  
 };
-app.use('/assets', express.static('public'));
 
-server.listen({ port }).then(({ url }) => {
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startApolloServer();
+
+app.listen(port, () => {
   connect();
-  console.log(`API RUNNING AT : ${url} :)`);// eslint-disable-line
+  console.log(`API RUNNING AT : http://localhost:${port} :)`);// eslint-disable-line
 });
