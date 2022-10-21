e39d7d4d2ee0f94180ad86b91a3684d0dc0aece2
sameh <sameh@ranadev.io>
2022-10-21 11:57:34 +0100

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server-express';
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
-import * as cors from 'cors';
+import cors from 'cors';
 import express from 'express';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import typeDefs from './schema/typeDefs';
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 4000;
 app.use(express.json());
-app.use(cors());
+app.use(cors);
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 const server = new ApolloServer({
