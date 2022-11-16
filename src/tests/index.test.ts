e43b2f8a10e5216a4da19f35f8d6d21eb0dc62b1
sameh <sameh@ranadev.io>
2022-11-16 10:44:28 +0100

    test login

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,43 @@
+import assert from 'node:assert';
 import server from '..';
 
 it('should validate user info correctly', async () => {
-  const response = await server.executeOperation({});
-  expect(response).toBeTruthy();
-  // expect(result?.errors).toBeTruthy();
+  // correct credentials
+  let response = await server.executeOperation({
+    query:
+      'mutation Login($email: String!, $password: String!) { login(email: $email, password: $password) { accessToken, user {email, id, username}}}',
+    variables: {
+      email: 'test@test.com',
+      password: 'test123',
+    },
+  });
+  assert(response.body.kind === 'single');
+  expect(response.body.singleResult.errors).toBeUndefined();
+  expect(response.body.singleResult.data).toBeTruthy();
+
+  // incorrect credentials
+  response = await server.executeOperation({
+    query:
+      'mutation Login($email: String!, $password: String!) { login(email: $email, password: $password) { accessToken, user {email, id, username}}}',
+    variables: {
+      email: 'incorrect@test.com',
+      password: 'test123',
+    },
+  });
+  assert(response.body.kind === 'single');
+  expect(response.body.singleResult.errors).toBeTruthy();
+  expect(response.body.singleResult.data).toBeFalsy();
+
+  // missing email
+  response = await server.executeOperation({
+    query:
+      'mutation Login($email: String!, $password: String!) { login(email: $email, password: $password) { accessToken, user {email, id, username}}}',
+    variables: {
+      email: '',
+      password: 'test123',
+    },
+  });
+  assert(response.body.kind === 'single');
+  expect(response.body.singleResult.errors).toBeTruthy();
+  expect(response.body.singleResult.data).toBeFalsy();
 });
