84673c622cb603cfab171efccf2cc19224a69396
sameh <sameh@ranadev.io>
2023-01-05 16:08:53 +0100

    fix authentication issue

--- src/utils/authenticated.ts
+++ src/utils/authenticated.ts
@@ -5,15 +5,11 @@ export default (token: string) => {
   if (!token) {
     throw new AuthenticationError('NOT_LOGGED_IN');
   }
-  const { userId, isAdmin } = verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET as string,
-    {
-      maxAge: '1d',
-    },
-  ) as JwtPayload;
-  if (!userId || !isAdmin) {
+  const { userId } = verify(token, process.env.ACCESS_TOKEN_SECRET as string, {
+    maxAge: '1d',
+  }) as JwtPayload;
+  if (!userId) {
     throw new AuthenticationError('INVALID_TOKEN');
   }
-  return { userId, isAdmin };
+  return { userId };
 };
