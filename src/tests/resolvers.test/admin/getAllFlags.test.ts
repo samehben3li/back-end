b07d629342b4ff47a5529cfd37743368ad0459ea
sameh11 <sameh@ranadev.io>
2023-02-02 20:38:17 +0100

    testing getAllFlags query with user token

--- src/tests/resolvers.test/admin/getAllFlags.test.ts
+++ src/tests/resolvers.test/admin/getAllFlags.test.ts
@@ -8,15 +8,21 @@ describe('Get_ALL_FLAGS', () => {
     server.close();
   });
   it('testing getAllFlags functionnality', async () => {
-    const { adminToken } = await getTokens();
+    const { adminToken, userToken } = await getTokens();
+
     // with admin access token
-    const response = await request(server)
-      .post('/')
-      .send(getAllFlagsQuery)
-      .set({
-        Authorization: adminToken,
-      });
+    let response = await request(server).post('/').send(getAllFlagsQuery).set({
+      Authorization: adminToken,
+    });
     expect(response?.body?.data?.getAllFlags).toBeTruthy();
     expect(response?.body?.errors).toBeUndefined();
+
+    // with user token
+    response = await request(server).post('/').send(getAllFlagsQuery).set({
+      Authorization: userToken,
+    });
+    expect(response?.body?.data).toBeNull();
+    expect(response?.body?.errors).toBeTruthy();
+    expect(response?.body?.errors[0]?.message).toBe('NOT_AUTHORIZED');
   });
 });
