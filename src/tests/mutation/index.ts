7d6f0055e16f4f7f67af8a076bb3c6f955997291
sameh11 <sameh@ranadev.io>
2023-02-03 19:56:36 +0100

    generate deleteUser mutation

--- src/tests/mutation/index.ts
+++ src/tests/mutation/index.ts
@@ -2,3 +2,4 @@ export { default as createUserMutation } from './createUserMutation';
 export { default as addFlagMutation } from './addFlagMutation';
 export { default as loginMutation } from './loginMutation';
 export { default as updateUserMutation } from './updateUserMutation';
+export { default as deleteUserMutation } from './deleteUserMutation';
